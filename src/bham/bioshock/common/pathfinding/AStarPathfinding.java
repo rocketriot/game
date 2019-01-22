a2f7d0cbf2d56d082022b1a43fc69946f323cfd3
tjr735 <tjr735@cs.bham.ac.uk>
2019-01-22 13:30:18 +0000

    Changes to PathfindingValues - added whether the point is passable and added an if statement to the constructor in this case. Also added some basic variables to A* Pathfinding

--- src/bham/bioshock/common/pathfinding/AStarPathfinding.java
+++ src/bham/bioshock/common/pathfinding/AStarPathfinding.java
@@ -2,5 +2,28 @@ package bham.bioshock.common.pathfinding;
 import bham.bioshock.common.consts.GridPoint;
 import bham.bioshock.common.models.*;
 
+// method to find a path between two points
 public class AStarPathfinding {
+
+    // start position of the path
+    private Coordinates startPosition;
+
+    // goal position of the path
+    private Coordinates goalPosition;
+
+    // current position of the path
+    private Coordinates currentPosition;
+
+    // the game grid at the time of the pathfinding search
+    private GridPoint[][] gameGrid;
+
+
+
+    public AStarPathfinding(GridPoint[][] grid, Coordinates startPosition, Coordinates goalPosition){
+        this.startPosition = startPosition;
+        currentPosition = startPosition;
+        this.goalPosition = goalPosition;
+        gameGrid = grid;
+    }
+
 }
--- src/bham/bioshock/common/pathfinding/PathfindingValues.java
+++ src/bham/bioshock/common/pathfinding/PathfindingValues.java
@@ -1,4 +1,5 @@
 package bham.bioshock.common.pathfinding;
+
 import bham.bioshock.common.models.Coordinates;
 
 // values that will be used during pathfinding - heuristics etc
@@ -13,16 +14,28 @@ public class PathfindingValues {
     // the parent point to this point - the point that came before
     private Coordinates parent;
 
+    // whether the point cam be moved through
+    private boolean passable;
+
     // initialise the values
-    public PathfindingValues(int pathCost, int totalCost, int xPos, int yPos) {
-        this.pathCost = pathCost;
-        this.totalCost = totalCost;
-        parent.setX(xPos);
-        parent.setY(yPos);
+    public PathfindingValues(int pathCost, int totalCost, int xPos, int yPos, boolean passable) {
+        if (passable){
+            this.pathCost = pathCost;
+            this.totalCost = totalCost;
+            parent.setX(xPos);
+            parent.setY(yPos);
+            this.passable = true;
+        } else {
+            this.pathCost = Integer.MAX_VALUE;
+            this.totalCost = Integer.MAX_VALUE;
+            parent.setX(xPos);
+            parent.setY(yPos);
+            this.passable = false;
+        }
     }
 
     // method to update the values
-    public void updateValues(int pathCost, int totalCost, int xPos, int yPos){
+    public void updateValues(int pathCost, int totalCost, int xPos, int yPos) {
         this.pathCost = pathCost;
         this.totalCost = totalCost;
         parent.setX(xPos);
@@ -30,18 +43,23 @@ public class PathfindingValues {
     }
 
     // method to get the pathCost
-    public int getPathCost(){
+    public int getPathCost() {
         return pathCost;
     }
 
     // method to get the totalCost
-    public int getTotalCost(){
+    public int getTotalCost() {
         return totalCost;
     }
 
-    //method to get the parent
-    public Coordinates getParent(){
+    // method to get the parent
+    public Coordinates getParent() {
         return parent;
+    }
+
+    //method to get whether the point is passable
+    public boolean getPassable() {
+        return passable;
     }
 
 }
