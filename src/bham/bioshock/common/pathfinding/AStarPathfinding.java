446b826b902428f86ddb871d08e9bb0d63e68715
tjr735 <tjr735@cs.bham.ac.uk>
2019-01-22 15:12:20 +0000

    Added A* methods to set up the grids for use, and some variables for max values

--- src/bham/bioshock/common/pathfinding/AStarPathfinding.java
+++ src/bham/bioshock/common/pathfinding/AStarPathfinding.java
@@ -21,19 +21,31 @@ public class AStarPathfinding {
     // the game grid at the time of the pathfinding search
     private GridPoint[][] gameGrid;
 
+    // the grid that will contain the pathfinding values for each point
+    private PathfindingValues[][] aStarGrid;
+
     // open list - list of all generated nodes
     private HashMap<Coordinates, PathfindingValues> openList;
 
     // closed list - list of all expanded nodes
     private HashMap<Coordinates, PathfindingValues> closedList;
 
-    public AStarPathfinding(GridPoint[][] grid, Coordinates startPosition, Coordinates goalPosition) {
+    // the maximum x value of the map
+    private int maxX;
+
+    // the maximum y value of the map
+    private int maxY;
+
+    public AStarPathfinding(GridPoint[][] grid, Coordinates startPosition, Coordinates goalPosition, int maxX, int maxY) {
         this.startPosition = startPosition;
         currentPosition = startPosition;
         this.goalPosition = goalPosition;
         gameGrid = grid;
-    }
+        this.maxX = maxX;
+        this.maxY = maxY;
 
+        setAStarGrid();
+    }
 
     // method to set the start position
     public void setStartPosition(Coordinates startPosition) {
@@ -49,6 +61,22 @@ public class AStarPathfinding {
     //method to set the game grid
     public void setGameGrid(GridPoint[][] grid) {
         gameGrid = grid;
+        setAStarGrid();
+    }
+
+    //method to setup the basics of the pathfindingValues grid
+    public void setAStarGrid() {
+
+        // go through the passed gameGrid and assign values accordingly to the aStarGrid
+        for (int x = 0; x < maxX; x++) {
+            for (int y = 0; y < maxY; y++) {
+                if (gameGrid[x][y].getType().equals("FUEL") || gameGrid[x][y].getType().equals("EMPTY")) {
+                    aStarGrid[x][y] = new PathfindingValues(0, 0, x, y, true);
+                } else {
+                    aStarGrid[x][y] = new PathfindingValues(0, 0, x, y, false);
+                }
+            }
+        }
     }
 
 }
