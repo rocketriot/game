b27913eeb66e03f725ec4cf325ab526cd7a8fd86
rxt711 <rxt711@cs.bham.ac.uk>
2019-01-23 01:11:55 +0000

    added background + testing camera and viewports

--- core/src/bham/bioshock/GameClass.java
+++ core/src/bham/bioshock/GameClass.java
@@ -3,31 +3,49 @@ package bham.bioshock;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class GameClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture background;
+	private OrthographicCamera camera;
+	private ExtendViewport viewport;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("ship.png");
+
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(1920, 1080, camera);
+		viewport.apply();
+
+		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2 , 0);
+		camera.update();
+
+		background = new Texture("game-background.png");
+	}
+
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+		batch.setProjectionMatrix(camera.combined);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		background.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
