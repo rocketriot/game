96f19833004b7b844a5c17501efa0ab119511c13
rxt711 <rxt711@student.bham.ac.uk>
2019-01-23 11:27:24 +0000

    Refactored render class to GameScreen and to fit with other UI classes

--- app/src/bham/bioshock/client/ui/GameScreen.java
+++ app/src/bham/bioshock/client/ui/GameScreen.java
@@ -2,27 +2,34 @@ package bham.bioshock.client.ui;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-public class Renderer extends ApplicationAdapter {
+public class GameScreen implements Screen {
     private SpriteBatch batch;
     private Texture background;
     private OrthographicCamera camera;
     private ExtendViewport viewport;
+    private SceneController scene_controller;
 
-    @Override
-    public void create() {
+    public GameScreen(final SceneController scene_controller) {
+        this.scene_controller = scene_controller;
         batch = new SpriteBatch();
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(1920, 1080, camera);
         viewport.apply();
 
-        background = new Texture("game-background.png");
+        background = new Texture(Gdx.files.internal("app/assets/game-background.png"));
+    }
+
+    @Override
+    public void show() {
+
     }
 
     public void resize(int width, int height) {
@@ -31,7 +38,22 @@ public class Renderer extends ApplicationAdapter {
     }
 
     @Override
-    public void render() {
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
--- app/src/bham/bioshock/client/ui/SceneController.java
+++ app/src/bham/bioshock/client/ui/SceneController.java
@@ -1,15 +1,19 @@
 package bham.bioshock.client.ui;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
 public class SceneController extends Game {
     private MainMenuScreen menu_screen;
     private LoadingScreen loading_screen;
     private HowToScreen howto_screen;
+    private GameScreen game_screen;
+
     @Override
 
     public void create() {
         loading_screen = new LoadingScreen(this);
+        game_screen = new GameScreen(this);
         menu_screen = new MainMenuScreen(this);
         howto_screen = new HowToScreen(this);
         setScreen(menu_screen);
@@ -25,6 +29,9 @@ public class SceneController extends Game {
                 break;
             case 2:
                 this.setScreen(howto_screen);
+                break;
+            case 3:
+                this.setScreen(game_screen);
                 break;
             default:
                 this.setScreen(menu_screen);
