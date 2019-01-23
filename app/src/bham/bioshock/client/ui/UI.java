ca523fce27b55ddfa536640b532e7601da0dc1c7
Pav Sidhu <pav@pavsidhu.com>
2019-01-23 10:24:49 +0000

    Refactor libgdx setup into project

---
+++ app/src/bham/bioshock/client/ui/UI.java
@@ -0,0 +1,34 @@
+package bham.bioshock.client.ui;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class UI extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture(Gdx.files.internal("app/assets/Ship.png"));
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+}
