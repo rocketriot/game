fb55c63d61c0b82d6ce8db9107b684153c0684c6
Pav Sidhu <pav@pavsidhu.com>
2019-01-25 13:56:36 +0000

    Add GameBoard to GameBoardController

--- app/src/bham/bioshock/client/controllers/GameBoardController.java
+++ app/src/bham/bioshock/client/controllers/GameBoardController.java
@@ -2,13 +2,23 @@ package bham.bioshock.client.controllers;
 
 import bham.bioshock.client.Client;
 import bham.bioshock.common.models.Model;
+import bham.bioshock.common.models.GameBoard;
 
 public class GameBoardController implements Controller {
     private Client client;
     private Model model;
+    private GameBoard gameBoard;
 
     public GameBoardController(Client client, Model model) {
         this.client = client;
         this.model = model;
+
+        gameBoard = model.getGameBoard();
+
+        try {
+            gameBoard.generateGrid();
+        } catch (Exception e) {
+            // Handle no players error
+        }
     }
 }
