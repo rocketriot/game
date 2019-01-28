f4e4bdb8b8ef9ae5d177c84b0d44121248ec761a
rxt711 <rxt711@cs.bham.ac.uk>
2019-01-28 18:29:44 +0000

    Added basic rendering for players on the gameboard

--- app/src/bham/bioshock/client/controllers/GameBoardController.java
+++ app/src/bham/bioshock/client/controllers/GameBoardController.java
@@ -1,6 +1,8 @@
 package bham.bioshock.client.controllers;
 
 import bham.bioshock.client.Client;
+import bham.bioshock.client.screens.GameBoardScreen;
+import bham.bioshock.common.models.Coordinates;
 import bham.bioshock.common.models.Model;
 import bham.bioshock.common.consts.GridPoint;
 import bham.bioshock.common.models.GameBoard;
@@ -15,12 +17,22 @@ public class GameBoardController implements Controller {
         this.client = client;
         this.model = model;
 
+        //TODO TEMP CODE REMOVE
+        Player p1 = new Player(new Coordinates(0, 0));
+        Player p2 = new Player(new Coordinates(0, 35));
+        Player p3 = new Player(new Coordinates(35, 35));
+        Player p4 = new Player(new Coordinates(35, 0));
+        Player[] players = {p1, p2, p3, p4};
+        model.createGameBoard(players);
+
         gameBoard = model.getGameBoard();
 
         try {
             gameBoard.generateGrid();
         } catch (Exception e) {
             // Handle no players error
+            System.err.println("No Players: ");
+            e.printStackTrace();
         }
     }
 
