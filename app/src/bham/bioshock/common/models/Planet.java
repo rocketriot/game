b3f29136dacedc7a376fbfa0e93c741117cbfcb4
rxt711 <rxt711@cs.bham.ac.uk>
2019-01-29 19:06:11 +0000

    added textureID and flag to check if the object has been drawn

--- app/src/bham/bioshock/common/models/Planet.java
+++ app/src/bham/bioshock/common/models/Planet.java
@@ -1,8 +1,13 @@
 package bham.bioshock.common.models;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Random;
 import java.util.UUID;
 
 import bham.bioshock.common.models.Player;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 
 /**
  * Stores the data of a player on the game board
@@ -20,14 +25,27 @@ public class Planet {
     /** If captured, specifies the player that has captured the planet */
     private Player playerCaptured = null;
 
+    /** Stores whether the object has been drawn this cycle */
+    private boolean drawn = false;
+
+    /** The texture ID for the object */
+    private int textureID;
+
     public Planet(String name, Coordinates coordinates) {
         this.id = UUID.randomUUID();
         this.name = name;
         this.coordinates = coordinates;
+        this.randomiseTexture();
     }
 
     public UUID getId() {
         return id;
+    }
+
+    private void randomiseTexture() {
+        FileHandle[] fh = Gdx.files.internal("app/assets/entities/planets").list();
+        Random r = new Random();
+        this.setTextureID(r.nextInt(fh.length));
     }
 
     public String getName() {
@@ -52,5 +70,21 @@ public class Planet {
 
     public void setPlayerCaptured(Player playerCaptured) {
         this.playerCaptured = playerCaptured;
+    }
+
+    public boolean isDrawn() {
+        return drawn;
+    }
+
+    public void setDrawn(boolean drawn) {
+        this.drawn = drawn;
+    }
+
+    public int getTextureID() {
+        return textureID;
+    }
+
+    public void setTextureID(int textureID) {
+        this.textureID = textureID;
     }
 }
