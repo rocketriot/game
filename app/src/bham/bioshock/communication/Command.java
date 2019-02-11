ecb4680347eca533e768d578c8d2f8e0b7a65a39
Pav Sidhu <pav@pavsidhu.com>
2019-02-11 23:03:25 +0000

    Add MOVE_PLAYER_ON_BOARD command

--- app/src/bham/bioshock/communication/Command.java
+++ app/src/bham/bioshock/communication/Command.java
@@ -1,13 +1,23 @@
 package bham.bioshock.communication;
 
 public enum Command {
+  // @formatter:off
+
+  // Connecting to a server
   COMM_DISCOVER,
   COMM_DISCOVER_RESPONSE,
   TEST,
+
+  // Creating a game
   ADD_PLAYER,
   START_GAME,
+
+  // Game board
   GET_GAME_BOARD,
-  UPDATE_GAME_BOARD;
+  UPDATE_GAME_BOARD,
+  MOVE_PLAYER_ON_BOARD;
+
+  // @formatter:on
 
   public byte[] getBytes() {
     return this.toString().getBytes();
