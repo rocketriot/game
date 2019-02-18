0fe5b903bc86676ae79f18d4d221efaa5cff0380
Monica Macovei <mxm1065@student.bham.ac.uk>
2019-02-18 10:23:38 +0000

    bullet class

---
+++ app/src/bham/bioshock/minigame/models/Bullet.java
@@ -0,0 +1,14 @@
+package bham.bioshock.minigame.models;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Bullet extends Entity {
+
+    public Bullet(float _x, float _y) {
+        super(_x, _y);
+    }
+
+    public TextureRegion getTexture() {
+        return null;
+    }
+}
