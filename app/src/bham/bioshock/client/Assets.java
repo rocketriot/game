199272ee064055aca89d6926199c653fe6c745cf
Pav Sidhu <pav@pavsidhu.com>
2019-02-22 17:13:55 +0000

    Make assets final

--- app/src/bham/bioshock/client/Assets.java
+++ app/src/bham/bioshock/client/Assets.java
@@ -1,17 +1,17 @@
 package bham.bioshock.client;
 
 public class Assets {
-  public static String gameBackground = "app/assets/backgrounds/game.png";
+  public static final String gameBackground = "app/assets/backgrounds/game.png";
   
-  public static String planetsFolder = "app/assets/entities/planets";
+  public static final String planetsFolder = "app/assets/entities/planets";
 
-  public static String playersFolder = "app/assets/entities/players";
+  public static final String playersFolder = "app/assets/entities/players";
 
-  public static String asteroidsFolder = "app/assets/entities/asteroids";
+  public static final String asteroidsFolder = "app/assets/entities/asteroids";
   
-  public static String fuel = "app/assets/entities/fuel.png";
+  public static final String fuel = "app/assets/entities/fuel.png";
   
-  public static String particleEffectsFolder = "app/assets/particle-effects";
+  public static final String particleEffectsFolder = "app/assets/particle-effects";
   
-  public static String particleEffect = "app/assets/particle-effects/rocket-trail.p";
+  public static final String particleEffect = "app/assets/particle-effects/rocket-trail.p";
 }
