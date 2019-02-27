7f78d704dedc0c2e25976898eea4e025c6d5896c
tjr735 <tjr735@cs.bham.ac.uk>
2019-02-27 16:22:04 +0000

    progress on writing changes to XML file

--- app/src/bham/bioshock/client/XMLInteraction.java
+++ app/src/bham/bioshock/client/XMLInteraction.java
@@ -23,13 +23,12 @@ public class XMLInteraction {
     boolean soundsEnabled = false;
     float soundsVolume = 0f;
 
-
     File xmlFile = new File("app/assets/Preferences/Preferences.XML");
     DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder documentBuilder;
 
-    // get the XML file as a document in memory
     try {
+      // get the XML file as a document in memory
       documentBuilder = dbFactory.newDocumentBuilder();
       Document document = documentBuilder.parse(xmlFile);
       document.getDocumentElement().normalize();
@@ -56,30 +55,89 @@ public class XMLInteraction {
               .getTextContent();
 
           // turn the strings into the types we want
-          if (musicEnabledString.equals("1")){
+          if (musicEnabledString.equals("1")) {
             musicEnabled = true;
-          }else{
+          } else {
             musicEnabled = false;
           }
           musicVolume = (float) (Integer.parseInt(musicVolumeString) / 100);
 
-          if (soundsEnabledString.equals("1")){
+          if (soundsEnabledString.equals("1")) {
             soundsEnabled = true;
-          }else{
+          } else {
             soundsEnabled = false;
           }
           soundsVolume = (float) (Integer.parseInt(soundsVolumeString) / 100);
         }
-
       }
-
     } catch (Exception e) {
       e.printStackTrace();
     }
     return new AppPreferences(musicEnabled, musicVolume, soundsEnabled, soundsVolume);
   }
 
-  public void preferencesToXML(){
+  /**
+   * Method to write the new preferences to the file once changes are complete
+   *
+   * @param musicEnabled Whether music should be saved as enabled or not
+   * @param musicVolume The music volume that should be saved
+   * @param soundsEnabled Whether sounds should be saved as enabled or not
+   * @param soundsVolume The sounds volume that should be saved
+   */
+  public void preferencesToXML(boolean musicEnabled, float musicVolume, boolean soundsEnabled,
+      float soundsVolume) {
+    File xmlFile = new File("app/assets/Preferences/Preferences.XML");
+
+    String musicEnabledString;
+    String musicVolumeString;
+    String soundsEnabledString;
+    String soundsVolumeString;
+
+    if (musicEnabled) {
+      musicEnabledString = "1";
+    } else {
+      musicEnabledString = "0";
+    }
+    musicVolumeString = String.valueOf((int) (musicVolume * 100));
+
+    if (soundsEnabled) {
+      soundsEnabledString = "1";
+    } else {
+      soundsEnabledString = "0";
+    }
+    soundsVolumeString = String.valueOf((int) (soundsVolume * 100));
+
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder documentBuilder;
+
+    try {
+      // get the XML file as a document in memory
+      documentBuilder = dbFactory.newDocumentBuilder();
+      Document document = documentBuilder.newDocument();
+      document.getDocumentElement().normalize();
+
+      NodeList nodeList = document.getElementsByTagName("sound");
+
+      for (int i = 0; i < nodeList.getLength(); i++) {
+        Element element = (Element) nodeList.item(i);
+
+        Node music_enabled = element.getElementsByTagName("music_enabled").item(0).getFirstChild();
+        music_enabled.setNodeValue(musicEnabledString);
+
+        Node music_volume = element.getElementsByTagName("music_volume").item(0).getFirstChild();
+        music_volume.setNodeValue(musicVolumeString);
+
+        Node sounds_enabled = element.getElementsByTagName("sounds_enabled").item(0)
+            .getFirstChild();
+        sounds_enabled.setNodeValue(soundsEnabledString);
+
+        Node sounds_volume = element.getElementsByTagName("sounds_volume").item(0).getFirstChild();
+        sounds_volume.setNodeValue(soundsVolumeString);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
 }
