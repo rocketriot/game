a0f6cf064bdc3716870bce1fa8e94e635ae55afd
Pav Sidhu <pav@pavsidhu.com>
2019-03-03 14:54:55 +0000

    Spacing in assets

--- app/src/bham/bioshock/client/Assets.java
+++ app/src/bham/bioshock/client/Assets.java
@@ -2,20 +2,12 @@ package bham.bioshock.client;
 
 public class Assets {
   public static final String gameBackground = "app/assets/backgrounds/game.png";
-  
   public static final String planetsFolder = "app/assets/entities/planets";
-  
   public static final String flagsFolder = "app/assets/entities/flags";
-
   public static final String playersFolder = "app/assets/entities/players";
-
   public static final String asteroidsFolder = "app/assets/entities/asteroids";
-  
   public static final String fuel = "app/assets/entities/fuel.png";
-  
   public static final String particleEffectsFolder = "app/assets/particle-effects";
-  
   public static final String particleEffect = "app/assets/particle-effects/rocket-trail.p";
-  
   public static final String turnPointer = "app/assets/ui/turn-pointer.png";
 }
