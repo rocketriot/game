2fae6019e2b4098956982707ea4f3af70d8c6203
Alexis Corney <axc680@student.bham.ac.uk>
2019-03-03 10:15:18 +0000

    changes

--- app/src/bham/bioshock/client/screens/JoinScreen.java
+++ app/src/bham/bioshock/client/screens/JoinScreen.java
@@ -28,6 +28,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 
 public class JoinScreen extends ScreenMaster {
@@ -504,7 +505,19 @@ public class JoinScreen extends ScreenMaster {
             Position bounds = checkBounds(pos.x, pos.y);
             pos = bounds;
 
+            router.call(Route.JOIN_SCREEN_MOVE, pos);
+
         }
 
     }
+
+    /**
+     *
+     * @param pos position of a player
+     * @param playerID the id of a player that will be moved
+     */
+    public void updateRocket(Position pos, UUID playerID) {
+
+    }
+
 }
