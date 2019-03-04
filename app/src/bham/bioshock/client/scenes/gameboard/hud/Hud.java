7bf2656d067af11750de96434d546ac2fe2c3350
Pav Sidhu <pav@pavsidhu.com>
2019-03-04 00:45:50 +0000

    Move Hud to different package

--- app/src/bham/bioshock/client/scenes/gameboard/hud/Hud.java
+++ app/src/bham/bioshock/client/scenes/gameboard/hud/Hud.java
@@ -1,4 +1,4 @@
-package bham.bioshock.client.scenes;
+package bham.bioshock.client.scenes.gameboard.hud;
 
 import bham.bioshock.client.Router;
 import bham.bioshock.client.scenes.gameboard.hud.FuelBar;
