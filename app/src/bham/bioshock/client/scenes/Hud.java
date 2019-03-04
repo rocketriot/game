cfbc38b1d2f318e08eca54c7b1f13186cd456cc6
Pav Sidhu <pav@pavsidhu.com>
2019-03-04 00:30:07 +0000

    Clean up HUD

--- app/src/bham/bioshock/client/scenes/Hud.java
+++ app/src/bham/bioshock/client/scenes/Hud.java
@@ -1,25 +1,16 @@
 package bham.bioshock.client.scenes;
 
-import bham.bioshock.client.Assets;
 import bham.bioshock.client.Router;
 import bham.bioshock.client.scenes.gameboard.hud.FuelBar;
 import bham.bioshock.client.scenes.gameboard.hud.PauseMenu;
 import bham.bioshock.client.scenes.gameboard.hud.ScoreBoard;
 import bham.bioshock.common.models.Player;
 import bham.bioshock.common.models.store.Store;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -31,10 +22,6 @@ public class Hud implements Disposable {
   public Stage stage;
   public FitViewport viewport;
   private Store store;
-  private Router router;
-
-  private ShapeRenderer sr;
-  private SpriteBatch batch;
 
   private FuelBar fuelBar;
   private ScoreBoard scoreBoard;
@@ -42,60 +29,18 @@ public class Hud implements Disposable {
   
   public Hud(SpriteBatch batch, Skin skin, int gameWidth, int gameHeight, Store store, Router router) {
     this.store = store;
-    this.router = router;
     this.skin = skin;
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-    this.batch = batch;
 
     OrthographicCamera camera = new OrthographicCamera();
     viewport = new FitViewport(this.gameWidth, this.gameHeight, camera);
     stage = new Stage(viewport, batch);
 
-    sr = new ShapeRenderer();
-
     pauseMenu = new PauseMenu(stage, batch, skin, router);
     scoreBoard = new ScoreBoard(stage, batch, skin);
     fuelBar = new FuelBar(stage, batch, skin);
   }
-
-  // private void setupTopBar() {
-  //   topBar = new HorizontalGroup();
-  //   topBar.setFillParent(true);
-  //   topBar.top();
-
-  //   // Adds widgets to the topBar
-  //   optionsMenu = new SelectBox(skin);
-  //   String[] menuOptions = {"Options Menu", "Settings", "Quit to main menu", "Quit to Desktop"};
-  //   optionsMenu.setItems(menuOptions);
-  //   optionsMenu.setSelected(menuOptions[0]);
-  //   topBar.addActor(optionsMenu);
-  //   topBar.setPosition(0, 0);
-  //   stage.addActor(topBar);
-
-  //   // Add listeners for each option
-  //   optionsMenu.addListener(
-  //       new ChangeListener() {
-  //         @Override
-  //         public void changed(ChangeEvent event, Actor actor) {
-  //           int selected = optionsMenu.getSelectedIndex();
-  //           switch (selected) {
-  //             case 1:
-  //               optionsMenu.setSelected(menuOptions[0]);
-  //               router.call(Route.PREFERENCES);
-  //               break;
-  //             case 2:
-  //               optionsMenu.setSelected(menuOptions[0]);
-  //               router.call(Route.MAIN_MENU);
-  //               break;
-  //             case 3:
-  //               optionsMenu.setSelected(menuOptions[0]);
-  //               Gdx.app.exit();
-  //               break;
-  //           }
-  //         }
-  //       });
-  // }
 
   // private void setupBottomBar() {
   //   HorizontalGroup bottomBar = new HorizontalGroup();
