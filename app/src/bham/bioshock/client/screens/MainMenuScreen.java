ddc2ff6865a80842bb2aa43e9b36b130002ebb13
Pav Sidhu <pav@pavsidhu.com>
2019-03-13 15:39:03 +0000

    Clean up

--- app/src/bham/bioshock/client/screens/MainMenuScreen.java
+++ app/src/bham/bioshock/client/screens/MainMenuScreen.java
@@ -9,11 +9,9 @@ import bham.bioshock.common.consts.Config;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 public class MainMenuScreen extends ScreenMaster {
   private Image logo;
