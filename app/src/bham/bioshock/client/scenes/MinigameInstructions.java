182a6d308951ee87fa617debd3c00ee2f4c40108
Monica Macovei <mxm1065@student.bham.ac.uk>
2019-03-15 12:52:02 +0000

    timer for 1 min

--- app/src/bham/bioshock/client/scenes/MinigameInstructions.java
+++ app/src/bham/bioshock/client/scenes/MinigameInstructions.java
@@ -8,68 +8,83 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.text.DecimalFormat;
+
 public class MinigameInstructions {
 
-        private SpriteBatch batch;
-        private Store store;
-        private BitmapFont font;
-        private boolean showPrompt = false;
-        private float duration;
-        private boolean displayed;
-        private FontGenerator fontGenerator;
-        private int fontSize = 72;
-
-        public MinigameInstructions(SpriteBatch batch, Store store) {
-            this.batch = batch;
-            this.store = store;
-            this.setup();
-        }
-
-    /**
-     * Generate the font
-     */
-    private void setup() {
-            fontGenerator = new FontGenerator();
-            font = fontGenerator.generate(fontSize, Color.WHITE);
-        }
-
-    /**
-     * Render the instructions once at the beginning
-     */
-    public void render() {
-            checkIfDisplayed();
-
-            if(showPrompt){
-                batch.begin();
-                int x = Config.GAME_WORLD_WIDTH / 8;
-                int y = Config.GAME_WORLD_HEIGHT / 2;
-
-                String instructions = store.getMinigameStore().getObjective().instructions();
-                font.draw(batch, instructions, x, y);
-                batch.end();
-
-                duration -= Gdx.graphics.getDeltaTime();
-
-                if (duration <= 0) {
-                    showPrompt = false;
-                } else {
-                    font = fontGenerator.generate(fontSize, new Color(1, 1, 1, duration));
-                }
-            }
-        }
+  private SpriteBatch batch;
+  private Store store;
+  private BitmapFont font;
+  private boolean showPrompt = false;
+  private float duration;
+  private boolean displayed;
+  private FontGenerator fontGenerator;
+  private int fontSize = 72;
+  private float timer = 0;
 
 
-    /**
-     * Checks if the minigame had started
-     * If yes sets the showpromt to true so the instructions will be displayed in render
-     */
-    private void checkIfDisplayed() {
-            if (!displayed && store.getMinigameStore().isStarted()) {
-                duration = 3f;
-                displayed = true;
-                showPrompt = true;
-            }
+  public MinigameInstructions(SpriteBatch batch, Store store) {
+    this.batch = batch;
+    this.store = store;
+    this.setup();
+  }
+
+  /**
+   * Generate the font
+   */
+  private void setup() {
+    fontGenerator = new FontGenerator();
+    font = fontGenerator.generate(fontSize, Color.WHITE);
+  }
+
+  /**
+   * Render the instructions once at the beginning
+   */
+  public void render() {
+    checkIfDisplayed();
+    displayTimer();
+    if (showPrompt) {
+      batch.begin();
+      int x = Config.GAME_WORLD_WIDTH / 8;
+      int y = Config.GAME_WORLD_HEIGHT / 2;
+
+      String instructions = store.getMinigameStore().getObjective().instructions();
+      font.draw(batch, instructions, x, y);
+      batch.end();
+
+      duration -= Gdx.graphics.getDeltaTime();
+
+      if (duration <= 0) {
+        showPrompt = false;
+      }
     }
+  }
+
+
+  /**
+   * Checks if the minigame had started
+   * If yes sets the showpromt to true so the instructions will be displayed in render
+   */
+  private void checkIfDisplayed() {
+    if (!displayed && store.getMinigameStore().isStarted()) {
+      duration = 3f;
+      displayed = true;
+      showPrompt = true;
+    }
+  }
+
+  private void displayTimer() {
+    int xTimer = Config.GAME_WORLD_WIDTH / 2;
+    int yTimer = Config.GAME_WORLD_HEIGHT;
+
+    timer += Gdx.graphics.getDeltaTime();
+
+    DecimalFormat df = new DecimalFormat("##");
+
+    batch.begin();
+    font.draw(batch, "00:" + df.format(60 - timer), xTimer, yTimer);
+    batch.end();
+  }
 }
 
 
