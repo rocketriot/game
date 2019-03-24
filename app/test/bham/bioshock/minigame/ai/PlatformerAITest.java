b6849d76b9b3a700407637b3df631de5642a95c7
Alexis Corney <axc680@student.bham.ac.uk>
2019-03-24 20:35:54 +0000

    starting JUnit tests

---
+++ app/test/bham/bioshock/minigame/ai/PlatformerAITest.java
@@ -0,0 +1,15 @@
+package bham.bioshock.minigame.ai;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlatformerAITest {
+
+  @org.junit.jupiter.api.Test
+  void run() {}
+
+  @org.junit.jupiter.api.Test
+  void afterUpdate() {}
+
+  @org.junit.jupiter.api.Test
+  void update() {}
+}
