93052f0ac17791c3fa8a3cf6c0c47e52bc9f7008
Pav Sidhu <pav@pavsidhu.com>
2019-03-26 13:23:14 +0000

    Fix turn text

--- app/src/bham/bioshock/client/scenes/gameboard/Notification.java
+++ app/src/bham/bioshock/client/scenes/gameboard/Notification.java
@@ -20,15 +20,15 @@ public class Notification {
   /** How long the current notification has been displayed */
   private float duration;
   
-  private boolean showTurnPrompt;
-  
-  private boolean wasMainPlayerTurn;
+  private boolean showTurn = false;
+ 
+  private boolean wasMainPlayerTurn = false;
 
   private String turnText = "It's your turn!";
 
-  private String winnerString;
+  private boolean showWinner = false;
   
-  private boolean showWinner;
+  private String winnerText;
 
   public Notification(SpriteBatch batch, Store store) {
     this.batch = batch;
@@ -43,70 +43,75 @@ public class Notification {
     renderMinigameEndNotifiation();
   }
 
+  private void drawNotification(String text) {
+    // Calculate coordinates of the text
+    float xOffset = fontGenerator.getOffset(font, turnText);
+    float x = (Config.GAME_WORLD_WIDTH / 2) - xOffset;
+    float y = Config.GAME_WORLD_HEIGHT / 2;
+    
+    // Draw text
+    batch.begin();
+    font.draw(batch, text, x, y);
+    batch.end();
+  }
+
   private void renderTurnNotifcation() {
     checkTurnChange();
 
-    if (store.isMainPlayersTurn() && showTurnPrompt) {
+    if (store.isMainPlayersTurn() && showTurn) {
       duration -= Gdx.graphics.getDeltaTime();
       
       // Hide turn prompt after duration
       if (duration <= 0)
-        showTurnPrompt = false;
+        showTurn = false;
 
       // Fade out text
       font.setColor(1, 1, 1, duration/2);
-
-      // Calculate coordinates of the text
-//      float xOffset = fontGenerator.getOffset(font, turnText);
-      float x = Config.GAME_WORLD_WIDTH / 2 ;
-      float y = Config.GAME_WORLD_HEIGHT / 2;
       
-      batch.begin();
-      font.draw(batch, turnText, x, y);
-      batch.end();
+      drawNotification(turnText);
     }
   }
 
-  /** sets showTurnPrompt to true if it has just switch to the main player's turn */
+  /** sets showTurn to true if it has just switch to the main player's turn */
   private void checkTurnChange() {
+    // Don't show the turn prompt is the minigame prompt is visible
+    if (showWinner) return;
     // If it's the main players turn show the notification
     if (store.isMainPlayersTurn() && !wasMainPlayerTurn) {
       duration = NOTIFICATION_DURATION;
       wasMainPlayerTurn = true;
+      showTurn = true;
     }
     
     // If it's no longer the main players turn reset the notification
-    if (!store.isMainPlayersTurn() && wasMainPlayerTurn)
+    if (!store.isMainPlayersTurn() && wasMainPlayerTurn) {
       wasMainPlayerTurn = false;
+    }
   }
 
   private void renderMinigameEndNotifiation() {
     checkMinigameEnd();
     
-    if (showWinner) {
-      batch.begin();
-      int x = Config.GAME_WORLD_WIDTH / 2 - winnerString.length() * 15;
-      int y = Config.GAME_WORLD_HEIGHT - 100;
-      font.draw(batch, winnerString, x, y);
-      batch.end();
+    if (!showWinner) return;
 
-      duration -= Gdx.graphics.getDeltaTime();
-      if (duration <= 0) {
-        showWinner = false;
-      } else {
-        font.setColor(1, 1, 1, duration/2);
-      }
-    }
+    duration -= Gdx.graphics.getDeltaTime();
+
+    if (duration <= 0)
+      showWinner = false;
+
+    font.setColor(1, 1, 1, duration / 2);
+
+    drawNotification(winnerText);
   }
 
   /** Creates the string to display if someone has just won a minigame and sets showWinner to true */
   private void checkMinigameEnd() {
-    if (store.getMinigameWinner() != null) {
-      duration = 3f;
-      winnerString = store.getMinigameWinner() + " won the minigame!";
-      showWinner = true;
-      store.setMinigameWinner(null);
-      font.setColor(1, 1, 1, 1);
-    }
+    if (store.getMinigameWinner() == null) return;
+
+    duration = 3f;
+    showWinner = true;
+    winnerText = store.getMinigameWinner() + " won the minigame!";
+    
+    store.setMinigameWinner(null);
   }
 }
